674fee7ce9dba729a3d066942b1eed1c5dfbba32
Apells1 <57453929+Apells1@users.noreply.github.com>
2021-10-30 17:16:49 -0500

    Add files via upload

---
+++ Morse1/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Morse1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        string Message = "";
+        string DotsAndDashs = "";
+
+        protected void onDot(object sender, EventArgs e)
+        {
+            DotsAndDashs += ".";
+            DotsAndDashes.Text = DotsAndDashs;
+        }
+        protected void onDash(object sender, EventArgs e)
+        {
+            DotsAndDashs += "-";
+            DotsAndDashes.Text = DotsAndDashs;
+        }
+        protected void onSpace(object sender, EventArgs e)
+        {
+            Message += Morse.MorseCoder(DotsAndDashs);
+            message.Text = Message;
+            DotsAndDashs += "";
+            DotsAndDashes.Text = DotsAndDashs;
+        }
+    }
+}
